59aba3fe401329b1d332bc225398bbe63b4392e4
Ujjwal <ujjwal26092002@gmail.com>
2021-06-01 19:46:54 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,21 @@ import '../styles/App.css';
 import React, { useState } from 'react';
 import {Menu, Container,Image } from "semantic-ui-react";
 import Home from './Home';
+import Compare from './Compare';
 function App() {
-  var [view,setView] =useState()
+
+  var [view,setView] =useState(<Home/>)
+  
   return (
     <div className="App">
       <Menu fixed='top' inverted>
         <Container>
           <Menu.Item as='a' header onClick={()=>{setView(<Home/>)}}>
             <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
-            Project Name
+            Stock Charts
           </Menu.Item>
-          <Menu.Item as='a' onClick={()=>{setView(<Home/>)}}>Home</Menu.Item>
+          <Menu.Item as='a' onClick={()=>{setView(<Home/>);}}>Home</Menu.Item>
+          <Menu.Item as='a' onClick={()=>{setView(<Compare/>);}}>Compare Stocks</Menu.Item>
         </Container>
       </Menu>
     {view}
---
+++ src/components/Compare.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Dropdown, Container } from "semantic-ui-react";
+function Compare(){
+    return (
+        <div>
+        <Container style={{ marginTop: '7em' }}>
+
+        </Container>
+        </div>
+    )
+}
+export default Compare;
