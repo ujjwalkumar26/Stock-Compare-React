0687842d04249dd9f277dccb45355d4efb7ffc59
Ujjwal <ujjwal26092002@gmail.com>
2021-06-02 01:24:05 +0530

    few changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import '../styles/App.css';
 import React, { useState } from 'react';
-import {Menu, Container,Image } from "semantic-ui-react";
+import {Menu, Container,Image,Segment, List } from "semantic-ui-react";
 import Home from './Home';
 import Compare from './Compare';
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Menu fixed='top' inverted>
         <Container>
           <Menu.Item as='a' header onClick={()=>{setView(<Home/>)}}>
-            <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+            <Image size='mini' src='../logo.png' style={{ marginRight: '1.5em' }} />
             Stock Charts
           </Menu.Item>
           <Menu.Item as='a' onClick={()=>{setView(<Home/>);}}>Home</Menu.Item>
@@ -20,6 +20,21 @@ function App() {
         </Container>
       </Menu>
     {view}
+    <Segment inverted vertical style={{ margin: '9em 0em 0em', padding: '2em 0em 2em 0em' }}>
+      <Container textAlign='center'>
+        <List horizontal inverted divided link size='small'>
+          <List.Item as='a' href='#' onClick={()=>{setView(<Home/>);}} >
+            Home
+          </List.Item>
+          <List.Item as='a' target="__blank" href='https://www.alphavantage.co/documentation/'>
+            API
+          </List.Item>
+          <List.Item as='a' target="__blank" href='https://github.com/ujjwalkumar26/Stock-Compare-React'>
+            GITHUB
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
     </div>
   );
 }
